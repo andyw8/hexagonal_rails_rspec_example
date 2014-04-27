15a96758a2ac8f92c824c0f86ad49864ef66b5ae
Andy Waite <github.aw@andywaite.com>
2014-04-27 20:29:36 +0200

    Minor cleanup

--- spec/lib/widget_creator_spec.rb
+++ spec/lib/widget_creator_spec.rb
@@ -2,9 +2,7 @@ require 'spec_helper'
 
 describe WidgetCreator do
   describe ".make" do
-    let(:listener) { double('listener') }
     let(:params) { double('params') }
-    let(:widget) { double('widget') }
 
     it "instantiates a new Widget with the given params" do
       widget = double('widget').as_null_object
@@ -16,7 +14,8 @@ describe WidgetCreator do
 
     context "on success" do
       it "notifies the listener" do
-        allow(widget).to receive(:save).and_return(true)
+        listener = double('listener')
+        widget = double('widget', save: true)
         allow(Widget).to receive(:new).and_return(widget)
 
         expect(listener).to receive(:make_succeeded).with(widget)
@@ -27,7 +26,8 @@ describe WidgetCreator do
 
     context "on failure" do
       it "notifies the listener" do
-        allow(widget).to receive(:save).and_return(false)
+        listener = double('listener')
+        widget = double('widget', save: false)
         allow(Widget).to receive(:new).and_return(widget)
 
         expect(listener).to receive(:make_failed).with(widget)
@@ -35,6 +35,5 @@ describe WidgetCreator do
         described_class.make(listener, params)
       end
     end
-
   end
 end
