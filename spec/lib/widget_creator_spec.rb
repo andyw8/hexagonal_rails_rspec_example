33da5174c37319a1fa20defdc683e1db8b6c7e62
Andy Waite <github.aw@andywaite.com>
2014-04-27 19:20:55 +0200

    Ensure widget is instantiated with correct params

--- spec/lib/widget_creator_spec.rb
+++ spec/lib/widget_creator_spec.rb
@@ -5,6 +5,15 @@ describe WidgetCreator do
     let(:listener) { double('listener') }
     let(:params) { double('params') }
 
+    it "instantiates a new Widget with the given params" do
+      widget = double('widget')
+      allow(widget).to receive(:save).and_return(true)
+      allow(listener).to receive(:make_succeeded)
+      expect(Widget).to receive(:new).with(params).and_return(widget)
+
+      described_class.make(listener, params)
+    end
+
     it "handles success" do
       widget = double('widget', save: true)
       allow(Widget).to receive(:new).and_return(widget)
