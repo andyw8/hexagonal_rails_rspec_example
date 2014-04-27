fa402f3e636a9f5b0e666c508aef3b8a500ec385
Andy Waite <github.aw@andywaite.com>
2014-04-27 19:33:59 +0200

    Refactor specs for clarity

--- spec/lib/widget_creator_spec.rb
+++ spec/lib/widget_creator_spec.rb
@@ -4,32 +4,37 @@ describe WidgetCreator do
   describe ".make" do
     let(:listener) { double('listener') }
     let(:params) { double('params') }
+    let(:widget) { double('widget') }
 
     it "instantiates a new Widget with the given params" do
-      widget = double('widget')
-      allow(widget).to receive(:save).and_return(true)
-      allow(listener).to receive(:make_succeeded)
+      widget = double('widget').as_null_object
+      listener = double('listener').as_null_object
+
       expect(Widget).to receive(:new).with(params).and_return(widget)
-
       described_class.make(listener, params)
     end
 
-    it "handles success" do
-      widget = double('widget', save: true)
-      allow(Widget).to receive(:new).and_return(widget)
+    context "on success" do
+      it "notifies the listener" do
+        allow(widget).to receive(:save).and_return(true)
+        allow(Widget).to receive(:new).and_return(widget)
 
-      expect(listener).to receive(:make_succeeded).with(widget)
+        expect(listener).to receive(:make_succeeded).with(widget)
 
-      described_class.make(listener, params)
+        described_class.make(listener, params)
+      end
     end
 
-    it "handles failure" do
-      widget = double('widget', save: false)
-      allow(Widget).to receive(:new).and_return(widget)
+    context "on failure" do
+      it "notifies the listener" do
+        allow(widget).to receive(:save).and_return(false)
+        allow(Widget).to receive(:new).and_return(widget)
 
-      expect(listener).to receive(:make_failed).with(widget)
+        expect(listener).to receive(:make_failed).with(widget)
 
-      described_class.make(listener, params)
+        described_class.make(listener, params)
+      end
     end
+
   end
 end
--- spec/lib/widget_updater_spec.rb
+++ spec/lib/widget_updater_spec.rb
@@ -6,20 +6,32 @@ describe WidgetUpdater do
     let(:params) { double('params') }
     let(:widget) { double('widget') }
 
-    it "handles success" do
-      allow(widget).to receive(:update).with(params).and_return(true)
+    it "attempts to update the widget with the given params" do
+      listener = double('listener').as_null_object
 
-      expect(listener).to receive(:update_succeeded).with(widget)
+      expect(widget).to receive(:update).with(params)
 
       described_class.update(listener, widget, params)
     end
 
-    it "handles failure" do
-      allow(widget).to receive(:update).with(params).and_return(false)
+    context "on success" do
+      it "notifies the listener" do
+        allow(widget).to receive(:update).with(params).and_return(true)
 
-      expect(listener).to receive(:update_failed).with(widget)
+        expect(listener).to receive(:update_succeeded).with(widget)
 
-      described_class.update(listener, widget, params)
+        described_class.update(listener, widget, params)
+      end
+    end
+
+    context "on failure" do
+      it "notifies the listener" do
+        allow(widget).to receive(:update).with(params).and_return(false)
+
+        expect(listener).to receive(:update_failed).with(widget)
+
+        described_class.update(listener, widget, params)
+      end
     end
   end
 end
