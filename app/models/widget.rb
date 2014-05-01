b7092258df05a65e4b65c851505a7b871375c930
Andy Waite <github.aw@andywaite.com>
2014-05-01 22:30:47 +0200

    Testing Hound CI

--- app/models/widget.rb
+++ app/models/widget.rb
@@ -1,3 +1,5 @@
+# Really long line to see if houndci.com is working.Really long line to see if houndci.com is working.Really long line to see if houndci.com is working.Really long line to see if houndci.com is working.
+
 class Widget < ActiveRecord::Base
   validates :name, presence: true
 end
